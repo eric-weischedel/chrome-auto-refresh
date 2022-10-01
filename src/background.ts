e5a1f7a89b29afcbdfe1f3483d5dd0ceb2cb0f6f
Eric Weischedel <yshuttle@gmail.com>
2022-10-01 18:48:52 -0400

    Use interfaces for message types

--- src/background.ts
+++ src/background.ts
@@ -1,30 +1,71 @@
-let intervalId: any | undefined = undefined;
-let intervalMs = 1000;
+export enum MessageType {
+  ENABLE,
+  DISABLE,
+  GET_CURRENT_STATUS,
+  SET_REFRESH_INTERVAL,
+}
 
-chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-  if (message === 'enable') {
-    clearInterval(intervalId);
-    intervalId = undefined;
-    const tab = await getCurrentTab();
-    intervalId = setInterval(() =>
-      chrome.scripting.executeScript({
-        target: { tabId: tab.id as number },
-        func: () => location.reload()
-      }),
-      intervalMs
-    );
-  } else if (message === 'disable') {
-    clearInterval(intervalId);
-    intervalId = undefined;
-  } else if (message === 'get-current-status') {
-    sendResponse({ enabled: !!intervalId, interval: intervalMs / 1000 });
-    return true;
-  } else if (message.type === 'set-refresh-interval') {
-    intervalMs = message.payload.interval * 1000;
+export interface Message<P = unknown> {
+  type: MessageType;
+  payload?: P;
+}
+
+export interface SetRefreshIntervalMessagePayload {
+  interval: number;
+}
+
+export interface GetCurrentStatusResponse {
+  enabled: boolean;
+  interval: number;
+}
+
+let intervalId: any | undefined = undefined;
+let intervalMs = 2000;
+
+chrome.runtime.onMessage.addListener(
+  async (message: Message, sender, sendResponse) => {
+    switch (message.type) {
+      case MessageType.ENABLE:
+        clearInterval(intervalId);
+        intervalId = undefined;
+        const tab = await getCurrentTab();
+        intervalId = setInterval(
+          () =>
+            chrome.scripting.executeScript({
+              target: { tabId: tab.id as number },
+              func: () => location.reload(),
+            }),
+          intervalMs
+        );
+        break;
+
+      case MessageType.DISABLE:
+        clearInterval(intervalId);
+        intervalId = undefined;
+        break;
+
+      case MessageType.GET_CURRENT_STATUS:
+        sendResponse({
+          enabled: !!intervalId,
+          interval: intervalMs / 1000,
+        } as GetCurrentStatusResponse);
+        return true;
+
+      case MessageType.SET_REFRESH_INTERVAL:
+        const payload = (message as Message<SetRefreshIntervalMessagePayload>)
+          .payload;
+        if (payload) {
+          intervalMs = payload.interval * 1000;
+        }
+        break;
+    }
   }
-});
+);
 
 async function getCurrentTab() {
-  const tabs = await chrome.tabs.query({ active: true, lastFocusedWindow: true })
+  const tabs = await chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true,
+  });
   return tabs[0];
 }
--- src/popup.ts
+++ src/popup.ts
@@ -1,15 +1,36 @@
-const autoRefreshEnabledToggle = document.getElementById("autoRefreshEnabledToggle") as HTMLInputElement;
-const refreshIntervalInput = document.getElementById("refreshIntervalInput") as HTMLInputElement;
+import {
+  GetCurrentStatusResponse,
+  Message,
+  MessageType,
+  SetRefreshIntervalMessagePayload,
+} from "./background";
 
-chrome.runtime.sendMessage('get-current-status', (response) => {
-  autoRefreshEnabledToggle.checked = response.enabled;
-  refreshIntervalInput.value = response.interval
-});
+const autoRefreshEnabledToggle = document.getElementById(
+  "autoRefreshEnabledToggle"
+) as HTMLInputElement;
+const refreshIntervalInput = document.getElementById(
+  "refreshIntervalInput"
+) as HTMLInputElement;
+
+chrome.runtime.sendMessage<Message>(
+  { type: MessageType.GET_CURRENT_STATUS },
+  (response: GetCurrentStatusResponse) => {
+    autoRefreshEnabledToggle.checked = response.enabled;
+    refreshIntervalInput.value = response.interval.toString();
+  }
+);
 
 autoRefreshEnabledToggle.addEventListener("click", (e) => {
-  chrome.runtime.sendMessage((e.target as HTMLInputElement).checked ? 'enable' : 'disable');
+  chrome.runtime.sendMessage<Message>({
+    type: (e.target as HTMLInputElement).checked
+      ? MessageType.ENABLE
+      : MessageType.DISABLE,
+  });
 });
 
 refreshIntervalInput.addEventListener("input", (e) => {
-  chrome.runtime.sendMessage({ type: 'set-refresh-interval', payload: { interval: (e.target as HTMLInputElement).value } })
-})
+  chrome.runtime.sendMessage<Message<SetRefreshIntervalMessagePayload>>({
+    type: MessageType.SET_REFRESH_INTERVAL,
+    payload: { interval: Number((e.target as HTMLInputElement).value) },
+  });
+});
